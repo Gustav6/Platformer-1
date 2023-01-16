765aced811b106f121a5e7406e539ef19361f1b5
SkieoXDD <mateusz.fryc@elev.ga.lbs.se>
2023-01-16 07:15:01 +0100

    Lightning

--- Assets/Scripts/Menus/MainMenuFunctions.cs
+++ Assets/Scripts/Menus/MainMenuFunctions.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuFunctions : MonoBehaviour
 {
+
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
